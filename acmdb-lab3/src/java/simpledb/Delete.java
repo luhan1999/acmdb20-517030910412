d6a44d1393380c3e7ec772c2f981633f45c16be6
luhan1999 <1992680852@qq.com>
2020-05-08 08:53:43 +0800

    lab3 - pass the test

--- acmdb-lab3/src/java/simpledb/Delete.java
+++ acmdb-lab3/src/java/simpledb/Delete.java
@@ -1,101 +1,3 @@
-//package simpledb;
-//
-//import java.io.IOException;
-//
-///**
-// * The delete operator. Delete reads tuples from its child operator and removes
-// * them from the table they belong to.
-// */
-//public class Delete extends Operator {
-//
-//    private static final long serialVersionUID = 1L;
-//
-//    private TransactionId tid;
-//    private DbIterator child;
-//    private TupleDesc tupleDesc;
-//    private int Count;
-//    /**
-//     * Constructor specifying the transaction that this delete belongs to as
-//     * well as the child to read from.
-//     *
-//     * @param t
-//     *            The transaction this delete runs in
-//     * @param child
-//     *            The child operator from which to read tuples for deletion
-//     */
-//    public Delete(TransactionId t, DbIterator child) {
-//        // some code goes here
-//        this.tid = t;
-//        this.child = child;
-//        this.tupleDesc = new TupleDesc(new Type[]{Type.INT_TYPE});
-//        this.Count = -1;
-//    }
-//
-//    public TupleDesc getTupleDesc() {
-//        // some code goes here
-//        return tupleDesc;
-//    }
-//
-//    public void open() throws DbException, TransactionAbortedException {
-//        // some code goes here
-//        super.open();
-//        child.open();
-//        Count = 0;
-//        while (child.hasNext()){
-//            Tuple next = child.next();
-//            try {
-//                Database.getBufferPool().deleteTuple(tid, next);
-//                Count ++;
-//            } catch (IOException e){
-//                e.printStackTrace();
-//            }
-//        }
-//    }
-//
-//    public void close() {
-//        // some code goes here
-//        Count = -1;
-//        child.close();
-//        super.close();
-//    }
-//
-//    public void rewind() throws DbException, TransactionAbortedException {
-//        // some code goes here
-//        this.close();
-//        this.open();
-//    }
-//
-//    /**
-//     * Deletes tuples as they are read from the child operator. Deletes are
-//     * processed via the buffer pool (which can be accessed via the
-//     * Database.getBufferPool() method.
-//     *
-//     * @return A 1-field tuple containing the number of deleted records.
-//     * @see Database#getBufferPool
-//     * @see BufferPool#deleteTuple
-//     */
-//    protected Tuple fetchNext() throws TransactionAbortedException, DbException {
-//        // some code goes here
-//        if (Count == -1) return null;
-//        Tuple deletenum = new Tuple(tupleDesc);
-//        deletenum.setField(0,new IntField(Count));
-//        Count = -1;
-//        return deletenum;
-//    }
-//
-//    @Override
-//    public DbIterator[] getChildren() {
-//        // some code goes here
-//        return new DbIterator[]{child};
-//    }
-//
-//    @Override
-//    public void setChildren(DbIterator[] children) {
-//        // some code goes here
-//        child = children[0];
-//    }
-//
-//}
 package simpledb;
 
 import java.io.IOException;
@@ -107,45 +9,60 @@ import java.io.IOException;
 public class Delete extends Operator {
 
     private static final long serialVersionUID = 1L;
+
     private TransactionId tid;
     private DbIterator child;
-    private TupleDesc schema;
-    private int counter = -1;
-
+    private TupleDesc tupleDesc;
+    private int Count;
     /**
      * Constructor specifying the transaction that this delete belongs to as
      * well as the child to read from.
      *
-     * @param t     The transaction this delete runs in
-     * @param child The child operator from which to read tuples for deletion
+     * @param t
+     *            The transaction this delete runs in
+     * @param child
+     *            The child operator from which to read tuples for deletion
      */
     public Delete(TransactionId t, DbIterator child) {
         // some code goes here
         this.tid = t;
         this.child = child;
-        this.schema = new TupleDesc(new Type[]{Type.INT_TYPE});
+        this.tupleDesc = new TupleDesc(new Type[]{Type.INT_TYPE});
+        this.Count = -1;
     }
 
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return schema;
+        return tupleDesc;
     }
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
         super.open();
         child.open();
+        Count = 0;
+        while (child.hasNext()){
+            Tuple next = child.next();
+            try {
+                Database.getBufferPool().deleteTuple(tid, next);
+                Count ++;
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void close() {
         // some code goes here
+        Count = -1;
         child.close();
         super.close();
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
         // some code goes here
-        child.rewind();
+        this.close();
+        this.open();
     }
 
     /**
@@ -159,20 +76,11 @@ public class Delete extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        if (counter != -1) return null;
-        counter = 0;
-        while (child.hasNext()) {
-            Tuple tup = child.next();
-            try {
-                Database.getBufferPool().deleteTuple(tid, tup);
-                ++counter;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        Tuple result = new Tuple(schema);
-        result.setField(0, new IntField(counter));
-        return result;
+        if (Count == -1) return null;
+        Tuple deletenum = new Tuple(tupleDesc);
+        deletenum.setField(0,new IntField(Count));
+        Count = -1;
+        return deletenum;
     }
 
     @Override
